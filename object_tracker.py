a65dab84a62b5aefe661c6988effa9a780daedeb
etienneshrdlu <73829148+etienneshrdlu@users.noreply.github.com>
2022-03-22 10:27:29 +0000

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -216,7 +216,7 @@ def main(_argv):
             color = [i * 255 for i in color]
             cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
             cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-            cv2.putText(frame, random_insult,(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+            cv2.putText(frame, random_insult,(int(bbox[0]), int(bbox[1]-10)),0, cv2.FONT_HERSHEY_DUPLEX, 0.75, (255,255,255),2)
 
         # if enable info flag then print details about each track
             if FLAGS.info:
